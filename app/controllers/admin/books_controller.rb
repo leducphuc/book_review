7127e913abcc7a0b61b6a3b7f8bd48060085c755
Luu Kha <luu.tuan.kha@framgia.com>
2016-08-02 17:19:39 +0700

    git finish book

---
+++ app/controllers/admin/books_controller.rb
@@ -0,0 +1,57 @@
+class Admin::BooksController < ApplicationController
+  load_and_authorize_resource
+  before_action :load_categories, only: [:new, :edit]
+
+  def new
+  end
+
+  def create
+    if @book.save
+      flash[:success] = t "book.create_notice"
+      redirect_to admin_book_path @book
+    else 
+      load_categories
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def index
+    @books = @books.page params[:page]
+  end  
+
+  def destroy
+    if @book.destroy
+      flash[:success] = t "book.destroy"
+    else 
+      flash.new[:danger] = t "book.destroy_alert"
+    end
+    redirect_to admin_books_path
+  end
+
+  def update
+    if @book.update book_params
+      flash[:success] = t "book.update_notice"
+      redirect_to admin_book_path @book
+    else
+      load_categories
+      render :edit
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit :title, :author, :category_id, 
+      :number_of_pages, :publish_date
+  end
+
+  def load_categories
+    @categories = Category.all
+    @category_selects = @categories.collect {|category| [category.name, category.id]}
+  end
+end
--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,9 @@ class Book < ActiveRecord::Base
   has_many :users, through: :user_books
 
   belongs_to :category
+
+  validates :title, presence: true, uniqueness: true, length: {maximum: 140}
+  validates :author, presence: true, length: {maximum: 50}
+  validates :number_of_pages, presence: true, numericality: {only_integer: true}, 
+    length: {maximum: 5}
 end
