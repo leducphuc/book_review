f5b0eb1f1887f5ba1380babfa7055c25f1e9cdf4
leducphuc <leducphuc1994@gmail.com>
2016-08-17 00:16:10 +0700

    add_comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :review
+  has_many :sub_comments, class_name: "Comment", foreign_key: "parent_id",
+    dependent: :destroy
+  belongs_to :parent_comment, class_name: "Comment"
 end
